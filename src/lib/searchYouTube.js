738e8ab605683ae989cc893c366dbfda01dc42cf
BrandonALittle <brandon.a.little@gmail.com>
2018-02-01 11:34:54 -0800

    added ajax request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,31 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  // search: function(options, callback) {
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      part: 'snippet',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      type: 'video',
+      videoEmbeddable: 'true'
+    },
+    contentType: 'application/json',
+    success: function(data) {
+      callback (data.items);
+    },
+    error: function (data) {
+      console.error('Failed to receive videos', data);
+    }
+  });
+
+
+  // },
+
+
 };
 
+
 window.searchYouTube = searchYouTube;
+
