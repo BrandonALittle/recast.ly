e28855ba3decd95a4772ea3ed39b036031706433
BrandonALittle <brandon.a.little@gmail.com>
2018-02-01 11:34:30 -0800

    no change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-// TODO: Render the `App` component to the DOM
-
-ReactDOM.render(new App(), document.getElementById('app'));
+// TODO: Render the `App` coReDOM
+ReactDOM.render(<App />, document.getElementById('app'));
