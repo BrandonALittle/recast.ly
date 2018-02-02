9754880aaf3a0396521c37b3de2e1b0436f64f04
BrandonALittle <brandon.a.little@gmail.com>
2018-02-01 19:31:29 -0800

    added all search functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,55 +4,65 @@ class App extends React.Component {
 
     this.state = {
       videos: [],
-      currentVideo: this.videos[0],
+      currentVideo: {},
       isLoaded: false
     };
 
     this.handleTitleClick = this.handleTitleClick.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleSearchResults = this.handleSearchResults.bind(this);
+    this.getVideos = this.getVideos.bind(this);
+  }
+
+  componentDidMount() {
+    this.getVideos('rickroll');
   }
 
   handleTitleClick(video) {
     this.setState({currentVideo: video});
   }
 
-  handleSearch(query) {
-    var options = {
+  getVideos(query) {
+    let options = {
       key: window.YOUTUBE_API_KEY,
       query: query,
       max: 5
     };
 
-    window.searchYouTube(options, this.handleSearchResults);
-  }
-
-  handleSearchResults(data) {
-    this.setState({videos: data});
+    this.props.searchYouTube(options, (data) => {
+      this.setState({
+        isLoaded: true,
+        videos: data,
+        currentVideo: data[0]
+      });
+    });
   }
 
   render() {
-    return (
-      <div>
-        <nav className="navbar">
-          <div className="col-md-6 offset-md-3">
-            <Search handler={this.handleSearch}/>
-          </div>
-        </nav>
-        <div className="row">
-          <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
-          </div>
-          <div className="col-md-5">
-            <div>
+    if (!this.state.isLoaded) {
+      return (
+        <div>Please hold...</div>
+      );
+    } else {
+      return (
+        <div>
+          <nav className="navbar" >
+            <div className="col-md-6 offset-md-3">
+              <Search getVideos={this.getVideos} getDebouncedVideos={_.debounce(this.getVideos, 500).bind(this)}/>
+            </div>
+          </nav>
+          <div className="row">
+            <div className="col-md-7">
+              <VideoPlayer video={this.state.currentVideo}/>
+            </div>
+            <div className="col-md-5">
               <VideoList videos={this.state.videos} handler={this.handleTitleClick}/>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
